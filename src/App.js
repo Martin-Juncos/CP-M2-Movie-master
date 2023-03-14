6c18d68c6eccfef42ed3bc1926dfe8a20479fd81
Carlos Martin Juncos <mjuncos@soyhenry.com>
2023-03-14 09:31:50 -0300

    cp-m2

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from "react";
+// import { Route } from "react-router-dom";
+// import CreateMovie from "./components/CreateMovie/CreateMovie";
+// import Home from "./components/Home/Home";
+// import MovieDetail from "./components/MovieDetail/MovieDetail";
+// import Nav from "./components/Nav/Nav";
+
+
+function App() {
+  return (
+    <div>      
+    </div>
+  );
+}
+export default App;
